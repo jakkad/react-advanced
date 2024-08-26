03c749d965d894c1cc51e40e20543d1fc6a30e89
Jake <Jake@whiz-lab.com>
2024-08-27 00:05:29 +0300

    Day8

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -2,33 +2,32 @@ import { data } from "../../../data.js";
 import { useState } from "react";
 
 const UseStateArray = () => {
-  const [people, setPerson] = useState(data);
-  function removeItem(id) {
-    const filteredList = people.filter((person) => {
-      return person.id !== id;
+  const [people, setPeople] = useState(data);
+
+  const removeItem = (id) => {
+    const NewPeople = people.filter((person) => {
+      return person.id != id;
     });
-    setPerson(filteredList);
-  }
-  function clearAll() {
-    setPerson([]);
-  }
+    setPeople(NewPeople);
+  };
+
+  const removeAllItmes = () => {
+    setPeople([]);
+  };
+
   return (
     <div>
       {people.map((person) => {
         return (
           <div key={person.id}>
-            <h4>{person.name}</h4>
-            <button
-              className="btn"
-              type="button"
-              onClick={() => removeItem(person.id)}
-            >
+            <h2>{person.name}</h2>
+            <button className="btn" onClick={() => removeItem(person.id)}>
               Remove
             </button>
           </div>
         );
       })}
-      <button className="btn btn-red" onClick={clearAll}>
+      <button className="btn" onClick={removeAllItmes}>
         Clear All
       </button>
     </div>
--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,30 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    hobby: "read books",
+  });
+
+  const showJohn = () => {
+    setPerson({
+      name: "John",
+      age: 30,
+      hobby: "Play video Games",
+    });
+  };
+  return (
+    <>
+      <h2>useState object example</h2>
+      <h3>{person.name}</h3>
+      <h4>{person.age}</h4>
+      <h4>Enjoys:{person.hobby}</h4>
+      <button className="btn" onClick={showJohn}>
+        Show John
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
